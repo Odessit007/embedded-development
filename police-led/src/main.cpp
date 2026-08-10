91c9be42d9ccf6f4b070822d5abacc71bb93a400
Odessit007 <Odessit007@users.noreply.github.com>
2026-08-10 03:06:44 +0300

    Address change requests for police-led

--- police-led/src/main.cpp
+++ police-led/src/main.cpp
@@ -2,6 +2,12 @@
 
 #define BLUE_LED_OUT 16
 #define RED_LED_OUT 17
+#define N_BLINKS 2
+#define START_GAP 1000
+#define GAP_STEP 200
+
+uint16_t current_gap = START_GAP;
+int16_t gap_delta = 0;
 
 void blink(uint8_t pin, uint16_t times, uint16_t delayTime);
 
@@ -26,24 +32,19 @@ void blink(uint8_t pin, uint16_t times, uint16_t delayTime) {
 }
 
 void loop() {
-  uint16_t start_gap = 1000;
-  uint16_t current_gap = start_gap;
-  int16_t gap_delta = 0;
-  while (1 == 1) {
-    if (current_gap >= start_gap) {
-      Serial.println("Increasing speed");
-      gap_delta = -200;
-    }
-    if (current_gap <= 200) {
-      Serial.println("Decreasing speed");
-      gap_delta = 200;
-    }
-    Serial.println("Blinking with the blue LED");
-    blink(BLUE_LED_OUT, 2, current_gap);
-    Serial.println("Blinking with the red LED");
-    blink(RED_LED_OUT, 2, current_gap);
-    current_gap += gap_delta;
-    Serial.print("Gap updated. Current gap = ");
-    Serial.println(current_gap);
+  if (current_gap >= START_GAP) {
+    Serial.println("Increasing speed");
+    gap_delta = -GAP_STEP;
   }
+  if (current_gap <= GAP_STEP) {
+    Serial.println("Decreasing speed");
+    gap_delta = GAP_STEP;
+  }
+  Serial.println("Blinking with the blue LED");
+  blink(BLUE_LED_OUT, 2, current_gap);
+  Serial.println("Blinking with the red LED");
+  blink(RED_LED_OUT, 2, current_gap);
+  current_gap += gap_delta;
+  Serial.print("Gap updated. Current gap = ");
+  Serial.println(current_gap);
 }
