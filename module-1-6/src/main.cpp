20ad80d146235896d833de568167547d85ef46ec
Odessit007 <Odessit007@users.noreply.github.com>
2026-08-16 02:23:48 +0300

    Add module-1-6

---
+++ module-1-6/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+#define ADC_IN 4
+#define LED_OUT 15
+#define ADC_MAX 4095.0f
+#define U_REF 3100.0f
+
+
+void setup() {
+    Serial.begin(115200);
+    analogReadResolution(12);
+    pinMode(ADC_IN, INPUT);
+    pinMode(LED_OUT, OUTPUT);
+
+    Serial.println();
+    Serial.println("ADP_RAW | U_calc | U_ADP | Похибка, %");
+}
+
+void loop() {
+    const float adp_raw = analogRead(ADC_IN) / 1000.0f;
+    const float volatgeCalculated = (adp_raw / ADC_MAX) * U_REF;
+    const float voltageReadMilliVolts = analogReadMilliVolts(ADC_IN) / 1000.0f;
+    const float delta = ((volatgeCalculated - voltageReadMilliVolts) / voltageReadMilliVolts) * 100.0f;
+
+    Serial.printf("%3.2fV | %3.2fV | %3.2fV | %3.2f%%\n", adp_raw, volatgeCalculated, voltageReadMilliVolts, delta);
+
+    delay(100);
+}
